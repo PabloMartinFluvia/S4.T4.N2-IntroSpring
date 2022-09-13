11eeaecc89da4f8c44521182a70b9fec1c96e7c7
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-13 09:57:19 +0200

    clean code

--- S04T01N02MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n02/S04T01N02MartinFluviaPablo/controllers/HelloWorldController.java
+++ S04T01N02MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n02/S04T01N02MartinFluviaPablo/controllers/HelloWorldController.java
@@ -1,6 +1,8 @@
 package cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n02.S04T01N02MartinFluviaPablo.controllers;
 
 import cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n02.S04T01N02MartinFluviaPablo.S04T01N02MartinFluviaPabloApplication;
+import java.util.Collections;
+import java.util.Map;
 import org.slf4j.Logger;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,20 +17,18 @@ public class HelloWorldController {
     private String msg;
     
     
-    @GetMapping("/HelloWorld")
-    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){        
+    @GetMapping(value="/HelloWorld")
+    public Map<String,String> saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){        
         msg = "Hola, " + nom + ". Estàs executant un projecte Gradle";
-        LOG.info("Reacting to get HelloWorld.");
-        testAccent();
-        return msg;
+        LOG.info("Reacting to get HelloWorld");
+        return Collections.singletonMap("response",msg);
     }
     
     @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
-    public String saluda2(@PathVariable(required=false) String nom){
+    public Map<String,String> saluda2(@PathVariable(required=false) String nom){
         msg = "Hola, " + nom + ". Estàs executant un projecte Gradle";
         LOG.info("Reacting to get HelloWorld2");
-        testAccent();
-        return msg;
+        return Collections.singletonMap("response",msg);
     }
     
     private void testAccent(){
